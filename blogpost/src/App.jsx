7772a38068d5f71fe48516bc6f30dcd8f0a57e41
Najibullah Habibullah <najeebullahkhan511@yahoo.com>
2024-02-05 14:41:11 +0100

    Created Pages and their routes

--- blogpost/src/App.jsx
+++ blogpost/src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-red-600 font-bold font-mono">Blog Post</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
