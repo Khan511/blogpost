8f83602e03b21ec8b17f79a074936f84e28e207d
Najibullah Habibullah <najeebullahkhan511@yahoo.com>
2024-02-21 16:20:53 +0100

    Created Redux persist

--- blogpost/src/redux/store.js
+++ blogpost/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import UserReducer from "./user/UserSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import persistReducer from "redux-persist/es/persistReducer";
+
+const rootRecucer = combineReducers({
+  user: UserReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistReducers = persistReducer(persistConfig, rootRecucer);
 
 export const store = configureStore({
-  reducer: {
-    user: UserReducer,
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({ serializableCheck: false });
   },
 });
+
+export const persistor = persistStore(store);
