494dfe30ec6324d8e40b5e66ac406e0c1193689a
Najibullah Habibullah <najeebullahkhan511@yahoo.com>
2024-02-28 15:53:41 +0100

    Created Protected Route.

---
+++ blogpost/src/components/protectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+};
+
+export default ProtectedRoute;
